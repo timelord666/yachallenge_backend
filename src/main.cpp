f2e1d5978087429f568ad986094412f64b88f84d
timelord666 <timemonster666@gmail.com>
2024-12-01 12:22:09 +0500

    feat: registered register

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <userver/clients/dns/component.hpp>
 #include <userver/clients/http/component.hpp>
 #include <userver/components/minimal_server_component_list.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
+#include <userver/storages/postgres/component.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
 
@@ -9,13 +11,14 @@
 #include "handlers/v1/register/register.hpp"
 
 int main(int argc, char* argv[]) {
-  auto component_list = userver::components::MinimalServerComponentList()
-                            .Append<userver::server::handlers::Ping>()
-                            .Append<userver::components::TestsuiteSupport>()
-                            .Append<userver::components::HttpClient>()
-                            .Append<userver::server::handlers::TestsControl>()
-                            .Append<userver::components::Postgres>("postgres-db-1")
-                            .Append<userver::clients::dns::Component>();
+  auto component_list =
+      userver::components::MinimalServerComponentList()
+          .Append<userver::server::handlers::Ping>()
+          .Append<userver::components::TestsuiteSupport>()
+          .Append<userver::components::HttpClient>()
+          .Append<userver::server::handlers::TestsControl>()
+          .Append<userver::components::Postgres>("postgres-db-1")
+          .Append<userver::clients::dns::Component>();
 
   ya_challenge::AppendHello(component_list);
   ya_challenge::AppendRegisterUser(component_list);

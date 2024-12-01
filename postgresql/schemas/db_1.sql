a440664ea3f54031caeb0bcc6c0f67e0f359234f
Bilol <abdulkhayevbilol@gmail.com>
2024-12-01 18:06:54 +0500

    Modified UUID Array at schema

--- postgresql/schemas/db_1.sql
+++ postgresql/schemas/db_1.sql
@@ -4,29 +4,29 @@ CREATE SCHEMA IF NOT EXISTS yaChallenge;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- 1. User table
-CREATE TABLE IF NOT EXISTS yaChallenge.user (
+CREATE TABLE IF NOT EXISTS yaChallenge.users (
     id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
     email VARCHAR(255) UNIQUE NOT NULL,
     nickname VARCHAR(100) UNIQUE NOT NULL,
     androidToken TEXT,
     password TEXT NOT NULL,
-    categories UUID[] DEFAULT ARRAY[]::UUID[]
+    categories TEXT[] DEFAULT ARRAY[]::TEXT[]
 );
 
 -- 2. Category table
-CREATE TABLE IF NOT EXISTS yaChallenge.category (
+CREATE TABLE IF NOT EXISTS yaChallenge.categories (
     id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
     imageUrl TEXT,
     title VARCHAR(255) UNIQUE NOT NULL
 );
 
 -- 3. Challenge table
-CREATE TABLE IF NOT EXISTS yaChallenge.challenge (
+CREATE TABLE IF NOT EXISTS yaChallenge.challenges (
     id TEXT PRIMARY KEY DEFAULT uuid_generate_v4(),
     title VARCHAR(255) NOT NULL, 
     imageUrl TEXT,
     description TEXT NOT NULL, 
-    category UUID REFERENCES yaChallenge.category (id) ON DELETE SET NULL,
+    category UUID REFERENCES yaChallenge.categories (id) ON DELETE SET NULL,
     score INT NOT NULL
 );
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS yaChallenge.completedChallenges (
     score INT NOT NULL,
     completedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (userId, challengeId),
-    FOREIGN KEY (userId) REFERENCES yaChallenge.user (id) ON DELETE CASCADE,
-    FOREIGN KEY (challengeId) REFERENCES yaChallenge.challenge (id) ON DELETE CASCADE
+    FOREIGN KEY (userId) REFERENCES yaChallenge.users (id) ON DELETE CASCADE,
+    FOREIGN KEY (challengeId) REFERENCES yaChallenge.challenges (id) ON DELETE CASCADE
 );
 

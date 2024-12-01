7de93e5de3bf646626a1f71bdcfe3bf4e9074770
timelord666 <timemonster666@gmail.com>
2024-12-01 12:22:00 +0500

    fix: fixed impl

--- src/handlers/v1/register/register.cpp
+++ src/handlers/v1/register/register.cpp
@@ -1,8 +1,16 @@
+#include "register.hpp"
+
+#include <fmt/format.h>
 #include <userver/crypto/hash.hpp>
 #include <userver/formats/json.hpp>
+#include <userver/storages/postgres/cluster.hpp>
+#include <userver/storages/postgres/component.hpp>
+
+#include <userver/clients/dns/component.hpp>
+#include <userver/components/component.hpp>
+
 #include <userver/server/handlers/http_handler_base.hpp>
 #include <userver/storages/postgres/cluster.hpp>
-
 namespace ya_challenge {
 namespace {
 class RegisterUser final : public userver::server::handlers::HttpHandlerBase {
@@ -13,38 +21,48 @@ class RegisterUser final : public userver::server::handlers::HttpHandlerBase {
                const userver::components::ComponentContext& context)
       : HttpHandlerBase(config, context),
         pg_cluster_(
-            context.FindComponent<userver::components::Postgres>("postgres-db")
+            context
+                .FindComponent<userver::components::Postgres>("postgres-db-1")
                 .GetCluster()) {}
 
   std::string HandleRequestThrow(
       const userver::server::http::HttpRequest& request,
       userver::server::request::RequestContext&) const override {
-    auto email = request.GetFormDataArg("email").value_or("");
-    auto password = request.GetFormDataArg("password").value_or("");
-    auto nickname = request.GetFormDataArg("nickname").value_or("");
+    auto request_body =
+        userver::formats::json::FromString(request.RequestBody());
+    auto email = request_body["email"].As<std::optional<std::string>>();
+    auto nickname = request_body["nickname"].As<std::optional<std::string>>();
+    auto password = request_body["password"].As<std::optional<std::string>>();
+    auto token = request_body["androidToken"].As<std::optional<std::string>>();
+    auto categories = request_body["categories"].As<std::optional<std::vector<std::string>>>();
 
-    if (email.empty() || password.empty()) {
-      throw userver::server::handlers::ClientError("Missing required fields");
+    if (!email|| !password || !nickname || !categories) {
+      auto& response = request.GetHttpResponse();
+      response.SetStatus(userver::server::http::HttpStatus::kBadRequest);
+      return {};
     }
 
-    auto hashed_password = userver::crypto::hash::Sha256(password);
+    auto hashed_password = userver::crypto::hash::Sha256(password.value());
     auto uuid = GenerateUuid();
 
     auto user_result = pg_cluster_->Execute(
         userver::storages::postgres::ClusterHostType::kMaster,
         "SELECT COUNT(*) FROM User WHERE Email = $1", email);
     if (user_result.AsSingleRow<int>() > 0) {
-      throw userver::server::handlers::ClientError("Email already registered");
+      auto& response = request.GetHttpResponse();
+      response.SetStatus(userver::server::http::HttpStatus::kBadRequest);
+      return {};
     }
 
     pg_cluster_->Execute(userver::storages::postgres::ClusterHostType::kMaster,
-                         "INSERT INTO User (UUID, Email, Password, Nickname) "
-                         "VALUES ($1, $2, $3, $4)",
-                         uuid, email, hashed_password, nickname);
+                         "INSERT INTO User (id, email, password, nickname, categories, androidToken) "
+                         "VALUES ($1, $2, $3, $4, $5, $6)",
+                         uuid, email.value(), hashed_password,
+                         nickname, categories.value(),
+                         token.value_or(nullptr));
 
     userver::formats::json::ValueBuilder response;
-    response["message"] = "Registration successful";
-    response["user_uuid"] = uuid;
+    response["id"] = uuid;
 
     return userver::formats::json::ToString(response.ExtractValue());
   }
